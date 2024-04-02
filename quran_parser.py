64b809e3f53d9a962dbfe7a21e58c869e13d8cb0
UsaidMalik <umalik2003@gmail.com>
2024-04-02 10:23:37 -0400

    added in parsing script and parsed file with an empty new line indicating a new ayat

--- quran_parser.py
+++ quran_parser.py
@@ -1,36 +1,10 @@
 from pyarabic import araby
 # documentation https://github.com/linuxscout/pyarabic/blob/master/doc/features.md
 
-singleLine = ""
-with open("quran-simple.txt", encoding='utf-8') as Quran:
-    singleLine = Quran.readline()
-    # this should be 
-
-letters, marks = araby.separate(singleLine)
-print(letters) # i want to see what this looks like unencoded
-# its cursed.
-print(marks) # same here
-
-print(letters.encode('utf8'))
-print(marks.encode('utf8'))\
-
-
-"""
-Cool stuff from the docs:
->>> from pyarabic import araby
->>> araby.separate(text)
-(u'\u0627\u0644\u0639\u0631\u0628\u064a\u0629', u'\u064e\u0652\u064e\u064e\u064e\u064e\u064f')
->>> letters, marks =araby.separate(text)
->>> print(letters.encode('utf8'))
-العربية
->>> print(marks.encode('utf8'))
->>> for m in marks:
-...     print(araby.name(m))
-فتحة
-سكون
-فتحة
-فتحة
-فتحة
-فتحة
-ضمة
-"""
+trainingfile = open("quran-training.txt", "w")
+with open("quran-simple-no-ayat-numbers.txt", encoding='utf-8') as Quran:
+    for line in Quran:
+        letters, marks = araby.separate(line)
+        for idx, letter in enumerate(letters):
+            trainingfile.write(f'{letter.encode("utf8")}\t{marks[idx].encode("utf8")}\n')
+        trainingfile.write("\n")
